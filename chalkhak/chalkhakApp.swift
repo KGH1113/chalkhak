4e3e73e77c5bb742a1693fadbac40feea9382379
KGH1113 <gangguhyeon1113@gmail.com>
2025-01-13 05:03:49 +0900

    Initial Commit

---
+++ chalkhak/chalkhakApp.swift
@@ -0,0 +1,17 @@
+//
+//  chalkhakApp.swift
+//  chalkhak
+//
+//  Created by 강구현 on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct chalkhakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chalkhakTests/chalkhakTests.swift
@@ -0,0 +1,17 @@
+//
+//  chalkhakTests.swift
+//  chalkhakTests
+//
+//  Created by 강구현 on 1/13/25.
+//
+
+import Testing
+@testable import chalkhak
+
+struct chalkhakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
